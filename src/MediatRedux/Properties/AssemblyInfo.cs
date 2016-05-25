de1e0f827bd6c03d6d9b702ac96157e5a507e12f
Sam Storie <storie@gmail.com>
2016-05-25 16:44:29 -0500

    Added GitVersionTask

--- src/MediatRedux/Properties/AssemblyInfo.cs
+++ src/MediatRedux/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MediatRedux")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple implementation of the redux pattern using MediatR")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MediatRedux")]
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6144ab19-00f4-43dc-bcc2-ce1145e78883")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
